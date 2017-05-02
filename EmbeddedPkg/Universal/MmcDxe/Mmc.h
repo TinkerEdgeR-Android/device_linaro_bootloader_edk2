shivamurthy/edk2

EraseBlockProtocol implementation
Signed-off-by: Shivamurthy Shastri <shivamurthy.shastri@linaro.org>

EmbeddedPkg/Universal/MmcDxe/Mmc.h
@@ -20,6 +20,7 @@
 
 #include <Protocol/DiskIo.h>
 #include <Protocol/BlockIo.h>
+#include <Protocol/EraseBlock.h>
 #include <Protocol/DevicePath.h>
 #include <Protocol/MmcHost.h>
 
@@ -322,6 +323,7 @@ typedef struct _MMC_HOST_INSTANCE {
 
   MMC_STATE                 State;
   EFI_BLOCK_IO_PROTOCOL     BlockIo;
+  EFI_ERASE_BLOCK_PROTOCOL  EraseBlockProtocol;
   CARD_INFO                 CardInfo;
   EFI_MMC_HOST_PROTOCOL     *MmcHost;
 
@@ -463,6 +465,38 @@ EFI_STATUS
 EFIAPI
 MmcFlushBlocks (
   IN EFI_BLOCK_IO_PROTOCOL  *This
+  );
+
+/**
+  Erase a specified number of device blocks.
+
+  This function implements EFI_ERASE_BLOCK_PROTOCOL.EraseBlocks().
+
+  @param  This       Indicates a pointer to the calling context.
+  @param  MediaId    The media ID that the erase request is for.
+  @param  Lba        The starting logical block address to be erased. The caller is
+                     responsible for erasing only legitimate locations.
+  @param  Token      A pointer to the token associated with the transaction.
+  @param  Size       The size in bytes to be erased. This must be a multiple of the
+                     physical block size of the device.
+
+  @retval EFI_SUCCESS           The erase request was queued if Event is not NULL. The data was
+                                erased correctly to the device if the Event is NULL.
+  @retval EFI_WRITE_PROTECTED   The device cannot be erased due to write protection.
+  @retval EFI_DEVICE_ERROR      The device reported an error while attempting to perform the erase operation.
+  @retval EFI_INVALID_PARAMETER The erase request contain LBAs that are not valid.
+  @retval EFI_NO_MEDIA          There is no media in the device.
+  @retval EFI_MEDIA_CHANGED     The MediaId is not for the current media.
+
+**/
+EFI_STATUS
+EFIAPI
+MmcEraseBlocks (
+  IN EFI_BLOCK_IO_PROTOCOL          *This,
+  IN UINT32                         MediaId,
+  IN EFI_LBA                        Lba,
+  IN OUT EFI_ERASE_BLOCK_TOKEN      *Token,
+  IN UINTN                          Size
   );
 
 EFI_STATUS

EmbeddedPkg/Universal/MmcDxe/MmcBlockIo.c
@@ -375,3 +375,56 @@ MmcFlushBlocks (
 {
   return EFI_SUCCESS;
 }
+
+EFI_STATUS
+EFIAPI
+MmcEraseBlocks (
+  IN EFI_BLOCK_IO_PROTOCOL          *This,
+  IN UINT32                         MediaId,
+  IN EFI_LBA                        Lba,
+  IN OUT EFI_ERASE_BLOCK_TOKEN      *Token,
+  IN UINTN                          Size
+  )
+{
+  EFI_STATUS             Status;
+  MMC_HOST_INSTANCE      *MmcHostInstance;
+  EFI_MMC_HOST_PROTOCOL  *MmcHost;
+  UINT32                 Response[4];
+  UINTN                  CmdArg;
+
+  MmcHostInstance = MMC_HOST_INSTANCE_FROM_BLOCK_IO_THIS (This);
+  ASSERT (MmcHostInstance != NULL);
+  MmcHost = MmcHostInstance->MmcHost;
+  ASSERT (MmcHost);
+
+  if (This->Media->ReadOnly == TRUE)
+     return EFI_WRITE_PROTECTED;
+
+// EFI_DEVICE_ERROR, EFI_INVALID_PARAMETER
+
+  if (!MmcHostInstance->BlockIo.Media->MediaPresent)
+     return EFI_NO_MEDIA;
+
+  if (This->Media->MediaId != MediaId)
+     return EFI_MEDIA_CHANGED;
+
+  CmdArg = Lba;
+  Status = MmcHost->SendCommand (MmcHost, MMC_CMD35, CmdArg);
+  if (!EFI_ERROR (Status)) {
+     MmcHost->ReceiveResponse (MmcHost, MMC_RESPONSE_TYPE_R1, Response);
+  }
+
+  CmdArg = Lba + Size;
+  Status = MmcHost->SendCommand (MmcHost, MMC_CMD36, CmdArg);
+  if (!EFI_ERROR (Status)) {
+     MmcHost->ReceiveResponse (MmcHost, MMC_RESPONSE_TYPE_R1, Response);
+  }
+
+  CmdArg = 0;
+  Status = MmcHost->SendCommand (MmcHost, MMC_CMD38, CmdArg);
+  if (!EFI_ERROR (Status)) {
+     MmcHost->ReceiveResponse (MmcHost, MMC_RESPONSE_TYPE_R1b, Response);
+  }
+
+  return EFI_SUCCESS;
+}

TinkerEdgeR-Android/device_linaro_bootloader_edk2

Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

EmbeddedPkg/Universal/MmcDxe/Mmc.h
@@ -20,6 +20,7 @@
 
 #include <Protocol/DiskIo.h>
 #include <Protocol/BlockIo.h>
+#include <Protocol/EraseBlock.h>
 #include <Protocol/DevicePath.h>
 #include <Protocol/MmcHost.h>
 
@@ -330,6 +331,7 @@ typedef struct _MMC_HOST_INSTANCE {
 
   MMC_STATE                 State;
   EFI_BLOCK_IO_PROTOCOL     BlockIo;
+  EFI_ERASE_BLOCK_PROTOCOL  EraseBlockProtocol;
   CARD_INFO                 CardInfo;
   EFI_MMC_HOST_PROTOCOL     *MmcHost;
 
@@ -339,6 +341,7 @@ typedef struct _MMC_HOST_INSTANCE {
 #define MMC_HOST_INSTANCE_SIGNATURE                 SIGNATURE_32('m', 'm', 'c', 'h')
 #define MMC_HOST_INSTANCE_FROM_BLOCK_IO_THIS(a)     CR (a, MMC_HOST_INSTANCE, BlockIo, MMC_HOST_INSTANCE_SIGNATURE)
 #define MMC_HOST_INSTANCE_FROM_LINK(a)              CR (a, MMC_HOST_INSTANCE, Link, MMC_HOST_INSTANCE_SIGNATURE)
+#define MMC_HOST_INSTANCE_FROM_ERASEBLK(a)          CR (a, MMC_HOST_INSTANCE, EraseBlockProtocol, MMC_HOST_INSTANCE_SIGNATURE)
 
 
 EFI_STATUS
@@ -471,6 +474,38 @@ EFI_STATUS
 EFIAPI
 MmcFlushBlocks (
   IN EFI_BLOCK_IO_PROTOCOL  *This
+  );
+
+/**
+  Erase a specified number of device blocks.
+
+  This function implements EFI_ERASE_BLOCK_PROTOCOL.EraseBlocks().
+
+  @param  This       Indicates a pointer to the calling context.
+  @param  MediaId    The media ID that the erase request is for.
+  @param  Lba        The starting logical block address to be erased. The caller is
+                     responsible for erasing only legitimate locations.
+  @param  Token      A pointer to the token associated with the transaction.
+  @param  Size       The size in bytes to be erased. This must be a multiple of the
+                     physical block size of the device.
+
+  @retval EFI_SUCCESS           The erase request was queued if Event is not NULL. The data was
+                                erased correctly to the device if the Event is NULL.
+  @retval EFI_WRITE_PROTECTED   The device cannot be erased due to write protection.
+  @retval EFI_DEVICE_ERROR      The device reported an error while attempting to perform the erase operation.
+  @retval EFI_INVALID_PARAMETER The erase request contain LBAs that are not valid.
+  @retval EFI_NO_MEDIA          There is no media in the device.
+  @retval EFI_MEDIA_CHANGED     The MediaId is not for the current media.
+
+**/
+EFI_STATUS
+EFIAPI
+MmcEraseBlocks (
+  IN EFI_ERASE_BLOCK_PROTOCOL       *This,
+  IN UINT32                         MediaId,
+  IN EFI_LBA                        Lba,
+  IN OUT EFI_ERASE_BLOCK_TOKEN      *Token,
+  IN UINTN                          Size
   );
 
 EFI_STATUS

EmbeddedPkg/Universal/MmcDxe/MmcBlockIo.c
@@ -376,3 +376,56 @@ MmcFlushBlocks (
 {
   return EFI_SUCCESS;
 }
+
+EFI_STATUS
+EFIAPI
+MmcEraseBlocks (
+  IN EFI_ERASE_BLOCK_PROTOCOL       *This,
+  IN UINT32                         MediaId,
+  IN EFI_LBA                        Lba,
+  IN OUT EFI_ERASE_BLOCK_TOKEN      *Token,
+  IN UINTN                          Size
+  )
+{
+  EFI_STATUS             Status;
+  MMC_HOST_INSTANCE      *MmcHostInstance;
+  EFI_MMC_HOST_PROTOCOL  *MmcHost;
+  UINT32                 Response[4];
+  UINTN                  CmdArg;
+
+  MmcHostInstance = MMC_HOST_INSTANCE_FROM_ERASEBLK (This);
+  ASSERT (MmcHostInstance != NULL);
+  MmcHost = MmcHostInstance->MmcHost;
+  ASSERT (MmcHost);
+
+  if (MmcHostInstance->BlockIo.Media->ReadOnly == TRUE)
+     return EFI_WRITE_PROTECTED;
+
+// EFI_DEVICE_ERROR, EFI_INVALID_PARAMETER
+
+  if (!MmcHostInstance->BlockIo.Media->MediaPresent)
+     return EFI_NO_MEDIA;
+
+  if (MmcHostInstance->BlockIo.Media->MediaId != MediaId)
+     return EFI_MEDIA_CHANGED;
+
+  CmdArg = Lba;
+  Status = MmcHost->SendCommand (MmcHost, MMC_CMD35, CmdArg);
+  if (!EFI_ERROR (Status)) {
+     MmcHost->ReceiveResponse (MmcHost, MMC_RESPONSE_TYPE_R1, Response);
+  }
+
+  CmdArg = Lba + Size;
+  Status = MmcHost->SendCommand (MmcHost, MMC_CMD36, CmdArg);
+  if (!EFI_ERROR (Status)) {
+     MmcHost->ReceiveResponse (MmcHost, MMC_RESPONSE_TYPE_R1, Response);
+  }
+
+  CmdArg = 0;
+  Status = MmcHost->SendCommand (MmcHost, MMC_CMD38, CmdArg);
+  if (!EFI_ERROR (Status)) {
+     MmcHost->ReceiveResponse (MmcHost, MMC_RESPONSE_TYPE_R1b, Response);
+  }
+
+  return EFI_SUCCESS;
+}