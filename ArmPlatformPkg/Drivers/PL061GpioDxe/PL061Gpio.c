5437b27498b99be3bb39257b3a004818105c6a68
Haojian Zhuang <haojian.zhuang@linaro.org>
2017-06-12 15:38:53 +0800

    PL061Gpio: fix the mask of Get
    Fix the mask of Get (). It should be GPIO_PIN_MASK.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- ArmPlatformPkg/Drivers/PL061GpioDxe/PL061Gpio.c
+++ ArmPlatformPkg/Drivers/PL061GpioDxe/PL061Gpio.c
@@ -186,7 +186,7 @@ Get (
     return EFI_INVALID_PARAMETER;
   }
 
-  if (PL061GetPins (RegisterBase + PL061_GPIO_DATA_REG, Offset)) {
+  if (PL061GetPins (RegisterBase + PL061_GPIO_DATA_REG, GPIO_PIN_MASK (Offset))) {
     *Value = 1;
   } else {
     *Value = 0;
